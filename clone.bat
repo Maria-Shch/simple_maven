367d2fc0c8f2fd437a4f533963829bca82268a82
Maria Shcherbatykh <mariashcherbatykh2001@gmail.com>
2021-11-24 13:44:30 +0400

    Added token to url

--- clone.bat
+++ clone.bat
@@ -1,4 +1,4 @@
 rd /s /q "C:\Users\maria\Documents\5_sem\Administration\lab 7\simple_maven"
 cd C:\Users\maria\Documents\5_sem\Administration\lab 7
-git clone https://github.com/Maria-Shch/simple_maven.git
+git clone https://Maria-Shch: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@github.com/Maria-Shch/simple_maven.git
 
