aab40ca9f842dafc942d15dee975a5d4be982e5a
Maria Shcherbatykh <mariashcherbatykh2001@gmail.com>
2021-11-24 13:32:15 +0400

    Revert "Revert "added files .bat""
    This reverts commit 40a8b29be8bf7ae05dbb922f050fc40408a16c63.

---
+++ clone.bat
@@ -0,0 +1,4 @@
+rd /s /q "C:\Users\maria\Documents\5_sem\Administration\lab 7\simple_maven"
+cd C:\Users\maria\Documents\5_sem\Administration\lab 7
+git clone https://github.com/Maria-Shch/simple_maven.git
+
